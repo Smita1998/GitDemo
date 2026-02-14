0fee0bc2b563cf1d2c4d3953ff58316bfe3c33c8
student <smitadevadiga1998@gmail.com>
2026-02-14 19:52:56 +0530

    first commit

---
+++ src/test/java/rahulshettyacademy/ExtentReports/ExtentReportDemo.java
@@ -0,0 +1,42 @@
+package rahulshettyacademy.ExtentReports;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ExtentReportDemo {
+	ExtentReports extent;
+	
+@BeforeTest
+public void config()
+{
+	//ExtentReports, ExtentSparkReporter
+	String path=System.getProperty("user.dir")+"\\reports\\index.html";
+	ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+	reporter.config().setReportName("Web Automation Result");
+	reporter.config().setDocumentTitle("Test Results");
+	
+	extent = new ExtentReports();
+	extent.attachReporter(reporter);
+	extent.setSystemInfo("Tester", "Smita");
+}
+	
+@Test
+public void initialDemo()
+{
+	extent.createTest("Initial Demo");
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://rahulshettyacademy.com/");
+	System.out.println(driver.getTitle());
+	extent.flush();
+}
+
+}
